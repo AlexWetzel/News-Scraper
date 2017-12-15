475aefd41a789d6ddf4f1c67cda2256113d28f89
AlexWetzel <wetzela@live.com>
2017-12-14 22:46:10 -0500

    Server running and rendering main page

--- server.js
+++ server.js
@@ -20,63 +20,87 @@ var request = require("request");
 
 var db = require("./models");
 
-mongoose.Promise = Promise;
-mongoose.connect("mongodb://localhost/RuneScrape", {
-  useMongoClient: true
+//Dependencies
+var express = require("express");
+var bodyParser = require("body-parser");
+
+//Use the environment port or 3000
+var PORT = process.env.PORT || 3000;
+
+var app = express();
+
+//Serve static files from the 'public' directory
+app.use(express.static("public"));
+
+//Use body parser
+app.use(bodyParser.urlencoded({ extended: false }));
+
+//Set Handlebars
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+//Require routes
+var routes = require("./routes/routes.js");
+
+app.use("/", routes);
+
+app.listen(PORT, function() {
+	console.log("App listening on PORT " + PORT);
 });
 
-// First, tell the console what server.js is doing
-console.log("\n==================================\n" +
-            "Welcome to RuneScrape!\n" +
-            "Attempting to scrape atricles from the RuneScape official site...\n");
-
-// Making a request for reddit's "webdev" board. The page's HTML is passed as the callback's third argument
-request("http://services.runescape.com/m=news/a=13/", function(error, response, html) {
-	if (error) throw error;
-
-	// console.log(html)
-  // Load the HTML into cheerio and save it to a variable
-  // '$' becomes a shorthand for cheerio's selector commands, much like jQuery's '$'
-  var $ = cheerio.load(html);
-
-  // With cheerio, find each p-tag with the "title" class
-  // (i: iterator. element: the current element)
 
 
-  $("#newsContent").find("article").each(function(i, element) {
-  	// console.log(element)
 
-  	
-    // Save the text of the element in a "title" variable
-    var title = $(element).find("h4").children().text().trim();
 
-    // In the currently selected element, look at its child elements (i.e., its a-tags),
-    // then save the values for any "href" attributes that the child elements may have
-    var link = $(element).find("p").children().attr("href");
+// mongoose.Promise = Promise;
+// mongoose.connect("mongodb://localhost/RuneScrape", {
+//   useMongoClient: true
+// });
 
-    var summary = $(element).find("p").children().remove(".readMore").end().text().trim();
+// // First, tell the console what server.js is doing
+// console.log("\n==================================\n" +
+//             "Welcome to RuneScrape!\n" +
+//             "Attempting to scrape atricles from the RuneScape official site...\n");
 
-       // console.log(summary);
+// // Making a request for reddit's "webdev" board. The page's HTML is passed as the callback's third argument
+// request("http://services.runescape.com/m=news/a=13/", function(error, response, html) {
+// 	if (error) throw error;
 
-    var article = {
-    	title: title,
-    	summary: summary,
-    	link: link
-    }
-    // Save these results in an object that we'll push into the results array we defined earlier
-    // results.push({
-    //   title: title,
-    // 
-    //   link: link
-    // });
-    console.log(article);
+// 	// console.log(html)
+//   // Load the HTML into cheerio and save it to a variable
+//   // '$' becomes a shorthand for cheerio's selector commands, much like jQuery's '$'
+//   var $ = cheerio.load(html);
 
-    db.Article.create(article).then(function(dbArticle) {
-      	// res.send("Rune Scraped!");
-    }).catch(function(err) {
-    	console.log(err);
-    	// res.json(err);
-    });
-  });
-  console.log("Scraping complete");
-});
+//   // With cheerio, find each p-tag with the "title" class
+//   // (i: iterator. element: the current element)
+
+
+//   $("#newsContent").find("article").each(function(i, element) {
+//     // Save the text of the element in a "title" variable
+//     var title = $(element).find("h4").children().text().trim();
+
+//     // In the currently selected element, look at its child elements (i.e., its a-tags),
+//     // then save the values for any "href" attributes that the child elements may have
+//     var link = $(element).find("p").children().attr("href");
+
+//     var summary = $(element).find("p").children().remove(".readMore").end().text().trim();
+
+//     var article = {
+//     	title: title,
+//     	summary: summary,
+//     	link: link
+//     }
+ 
+//     console.log(article);
+
+//     db.Article.create(article).then(function(dbArticle) {
+//       	// res.send("Rune Scraped!");
+//     }).catch(function(err) {
+//     	console.log(err);
+//     	// res.json(err);
+//     });
+//   });
+//   console.log("Scraping complete");
+// });
